1ed7256a362e3da6c66d28cea811e3c42edd3e53
inhere <in.798@qq.com>
2021-05-15 23:49:44 +0800

    fix some script error. backup baidu push scirpt

---
+++ scripts/deploy.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -ie
+
+# 生成静态文件
+# npm run build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+# deploy to github pages
+echo 'inhere.xyz' > CNAME
+
+msg='auto deploy by github actions'
+githubUrl=https://inhere:${GITHUB_TOKEN}@github.com/inhere/inhere.github.io.git
+
+git config --global user.name "inhere"
+git config --global user.email "in.798@qq.com"
+
+git init
+git checkout -b main # goto branch main
+git add -A
+git commit -m "${msg}"
+# git push -f $githubUrl main:gh-pages # 推送到github gh-pages分支
+git push -f $githubUrl main:gh-pages # 推送到github gh-pages分支
+
+# deploy to coding pages
+# echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
+# echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
+
+# if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
+#   codingUrl=git@e.coding.net:xgy/xgy.git
+# else
+#   codingUrl=https://HmuzsGrGQX:${CODING_TOKEN}@e.coding.net/xgy/xgy.git
+# fi
+# git add -A
+# git commit -m "${msg}"
+# git push -f $codingUrl master # 推送到coding
+
+cd -
+rm -rf docs/.vuepress/dist
