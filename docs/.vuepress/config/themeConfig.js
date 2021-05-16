15f415ea0c274892922abef9198c4a8526b40391
inhere <in.798@qq.com>
2021-05-16 22:29:48 +0800

    fix repo name error

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -7,7 +7,7 @@ module.exports = {
   nav,
   sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
   logo: '/img/inhere-s03.png', // 导航栏logo
-  repo: 'inhere/inher.github.io', // 导航栏右侧生成Github链接
+  repo: 'inhere/inhere.github.io', // 导航栏右侧生成Github链接
   searchMaxSuggestions: 10, // 搜索结果显示最大数
   lastUpdated: 'Last Update', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
   docsDir: 'docs', // 编辑的文件夹
