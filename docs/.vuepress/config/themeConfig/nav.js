a5b2b7687e8de34f27727b5dad39992c736ab2de
inhere <in.798@qq.com>
2021-07-29 12:58:34 +0800

    update doc Signed-off-by: inhere <in.798@qq.com>

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -35,7 +35,7 @@ module.exports = [
     items: [
       { text: '技术文档', link: '/pages/9a7ee40fc232253e/' },
       { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
-      { text: 'Nodejs', link: '/pages/117708e0af7f0bd9/' },
+      { text: 'Linux工具', link: '/pages/zsv1okmiz2s9/' },
       { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
     ],
   },
